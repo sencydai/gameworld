01519d701d9cfac4d0322d0742e311c5f038a6fd
sencydai <312905904@qq.com>
2018-08-20 20:16:30 +0800

    update

--- stop.sh
+++ stop.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 pwd=`pwd`
 name=`pwd`/gameworld
-kill -2 `ps axu | grep $name |grep -v grep| awk '{print $2}'
+kill -2 `ps axu | grep $name |grep -v grep| awk '{print $2}'`
 
-echo "kill "$name""
+echo "kill "$name" "
