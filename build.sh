e04beecc590d6641737ad462db10fad75b5720e7
sencydai <312905904@qq.com>
2018-08-20 15:44:52 +0800

    update

--- build.sh
+++ build.sh
@@ -37,7 +37,7 @@ gitPull() {
 }
 
 goBuild() {
-    buildResult=`go build -o "${targetDir}/${targetFile}" "$baseProjectDir" 2>&1`
+    buildResult=`go build -o "${targetDir}/${targetFile}" "$buildPkg" 2>&1`
 
     if [ -z "$buildResult" ]; then
       buildResult="success"
