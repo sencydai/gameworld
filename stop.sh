44006638caf80fe9ce70e4ad60c536231abf5b8f
sencydai <312905904@qq.com>
2018-08-20 17:58:40 +0800

    update

---
+++ stop.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+pwd=`pwd`
+name=`pwd`/gameworld
+pid=`ps aux |grep $name|grep -v grep|grep -v "/bin/bash"| awk '{print $2}'`
+echo "pid=${pid}"
+
+if [-n "$pid"];then
+  kill -s 2 $pid
+  
+  pid=`ps aux |grep $name|grep -v grep|grep -v "/bin/bash"| awk '{print $2}'`
+  while [-n "$pid"];do
+	sleep 1
+  done
+fi
+
+echo "kill "$name"
